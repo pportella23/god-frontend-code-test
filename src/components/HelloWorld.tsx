d60217c88cd0fd2600d5a6e7d3a8c348fadb5abb
Pedro Portella <pportella23@gmail.com>
2023-10-19 17:09:03 -0300

    feat: create spacer component

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Block, Button } from "vcc-ui";
+import { Text } from "vcc-ui";
 import { useCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
+
+import styles from "../../public/css/home.module.css";
+import { Spacer } from "./Spacer";
 
 export const HelloWorld: React.FC = () => {
   const { cars } = useCars();
 
-  console.log(cars);
-
   return (
-    <Block extend={{ padding: 20 }}>
-      {cars.map((car) => (
-        <CarCard key={car.id} car={car}></CarCard>
-      ))}
-    </Block>
+    <div className={styles.homeWrapper}>
+      <Text variant="cook">Todos os modelos Recharge</Text>
+      <Spacer></Spacer>
+      <div className={styles.cardsWrapper}>
+        {cars.map((car) => (
+          <CarCard key={car.id} car={car}></CarCard>
+        ))}
+      </div>
+    </div>
   );
 };
