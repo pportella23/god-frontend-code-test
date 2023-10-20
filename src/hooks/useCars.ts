8009d6aae97262b5b2107378c2be19c6969f3164
Pedro Portella <pportella23@gmail.com>
2023-10-20 20:53:50 -0300

    feat: include env vars for local and prod

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Car } from "../types/car.interface";
+import { BASE_URL } from "../constants";
 
 export function useCars() {
   const [cars, setCars] = useState<Car[]>([]);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/cars").then((res) => {
+    axios.get(`${BASE_URL}/api/cars`).then((res) => {
       setCars(res.data);
     });
   }, []);
